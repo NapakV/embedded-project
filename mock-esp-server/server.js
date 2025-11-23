3d80538176a61c02ac0614727191facd7fdfc884
6733060021 <6733060021@student.chula.ac.th>
2025-11-23 23:17:46 +0700

    config extracted

--- mock-esp-server/server.js
+++ mock-esp-server/server.js
@@ -4,9 +4,10 @@ const fs = require('fs-extra');
 const path = require('path');
 const cors = require('cors');
 const mime = require('mime-types');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT | 5001
 
 // Middleware
 app.use(cors());
